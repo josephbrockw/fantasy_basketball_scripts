dec9cedf4e0a74b3501b3c0a690aee1879dcb050
Joe Wilkinson <me@thejoewilkinson.com>
2021-05-18 09:16:08 -0500

    finishing touches, readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Draft Lottery Script
+To run the draft lottery:
+`python draft_lottery.py`
+
+To run 1000000 simulations and print out the average pick for each team, run:
+`python draft_lottery.py sim`
--- draft_lottery.py
+++ draft_lottery.py
@@ -36,68 +36,64 @@ def run_lottery():
             order.append(i[0])
     return order
 
-# order = run_lottery()
+def run_simulations():
+    simulations = []
+    s = 1000000
+    for i in tqdm(range(s)):
+        simulations.append(run_lottery())
 
-# print(order)
-# order.reverse()
+    test = [
+        [1, []],
+        [2, []],
+        [3, []],
+        [4, []],
+        [5, []],
+        [6, []],
+        [7, []],
+        [8, []],
+        [9, []],
+        [10, []],
+        [11, []],
+        [12, []]
+    ]
 
-# for i in range(len(order)):
-#     print(f"{12 - i}: {order[i]}")
-#     sleep(1)
+    for sim in tqdm(simulations):
+        pick = False
+        for i in range(len(sim)):
+            test[i][1].append(sim.index(test[i][0]))
 
+    def calculate_odds(sims, pick):
+        not_pick = [x for x in sims if x > pick]
+        return round(1 - (len(not_pick) / len(sims)), 3)
 
-simulations = []
-s = 1000000
-for i in tqdm(range(s)):
-    simulations.append(run_lottery())
-
-test = [
-    [1, []],
-    [2, []],
-    [3, []],
-    [4, []],
-    [5, []],
-    [6, []],
-    [7, []],
-    [8, []],
-    [9, []],
-    [10, []],
-    [11, []],
-    [12, []]
-]
-
-joe = []
-
-for sim in tqdm(simulations):
-    pick = False
-    for i in range(len(sim)):
-        test[i][1].append(sim.index(test[i][0]))
-        if sim[i] in [2, 4] and pick == False:
-            joe.append(i + 1)
-            pick = True
-
-def calculate_odds(sims, pick):
-    not_pick = [x for x in sims if x > pick]
-    return round(1 - (len(not_pick) / len(sims)), 3)
-
-print("standing: average pick")
-for i in test:
-    print(f"{i[0]}: {sum(i[1]) / len(i[1])}")
+    print("standing: average pick")
+    for i in test:
+        print(f"{i[0]}: {sum(i[1]) / len(i[1])}")
 
 
-print("Simulations: ", len(joe))
+    print("Simulations: ", len(simulations))
 
-print(f"Average Position: {sum(joe) / len(joe)}")
-print(f"1: {calculate_odds(joe, 1)}")
-print(f"2: {calculate_odds(joe, 2)}")
-print(f"3: {calculate_odds(joe, 3)}")
-print(f"4: {calculate_odds(joe, 4)}")
-print(f"5: {calculate_odds(joe, 5)}")
-print(f"6: {calculate_odds(joe, 6)}")
-print(f"7: {calculate_odds(joe, 7)}")
-print(f"8: {calculate_odds(joe, 8)}")
-print(f"9: {calculate_odds(joe, 9)}")
-print(f"10: {calculate_odds(joe, 10)}")
-print(f"11: {calculate_odds(joe, 11)}")
-print(f"12: {calculate_odds(joe, 12)}")
+def draft_lottery(simulation=False):
+    if simulation:
+        run_simulations()
+    else:
+        order = run_lottery()
+        draft = []
+        for i, pick in enumerate(order):
+            draft.append((i + 1, pick))
+        draft.reverse()
+        print("Pick | Team")
+        print("-----------")
+        for pick in draft:
+            if pick[0] < 10:
+                print(f"{pick[0]}    | {pick[1]}")
+            else:
+                print(f"{pick[0]}   | {pick[1]}")
+            sleep(5)
+
+import sys
+if sys.argv[1] == "sim":
+    draft_lottery(simulation=True)
+else:
+    draft_lottery()
 
